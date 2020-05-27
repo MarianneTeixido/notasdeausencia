d0ab45c833e47dbe74ef7d53b706098adbe8456b
em <em@y530.ipv6.infinitum.net.mx>
2020-05-26 21:25:45 -0500

    escena 2 raycast

--- webserver/static/js/escena2new.js
+++ webserver/static/js/escena2new.js
@@ -9,7 +9,8 @@ const escena2 = {
 	this.animate();
 	this.initControls();
 	this.initRaycaster(); 
-
+	this.render(); 
+	
 	// this.clock = new THREE.Clock();
 	window.addEventListener('resize', this.handleResize); 
 	
@@ -19,24 +20,35 @@ const escena2 = {
     tam: [],
     esfericas: true,
     aleatorias: false,
+    //cube: {},
+    // text: {}, 
     cube: [],
     text: [],
 
-
     initScene: function(){
+
+	//this.container = document.createElement( 'div' );
+	//document.body.appendChild( this.container );
+
+	this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 10000 );
+	this.camera.position.z = 10;
 	
 	this.scene = new THREE.Scene();
-	this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-	this.camera.position.z = 10;
 
 	this.mouse = new THREE.Vector2(),
 
+	this.raycaster = new THREE.Raycaster();
+
+	
 	this.renderer = new THREE.WebGLRenderer({antialias: true});
 	this.renderer.setSize( window.innerWidth, window.innerHeight );
 	
-	document.body.appendChild( this.renderer.domElement );
+	 document.body.appendChild( this.renderer.domElement );
+	//this.container.appendChild( this.renderer.domElement );
+
+	window.document.addEventListener( 'mousemove', this.handleMouseMove, false);
+
 	
-	document.body.addEventListener("keyup", this.handleKeyUp);
 	//document.body.addEventListener( 'mousemove', this.onDocumentMouseMove, false );
 
     },
@@ -49,6 +61,7 @@ const escena2 = {
 	this.controls.screenSpacePanning = false;
 
 	//document.body.addEventListener( 'keyup', this.handleKeyup);
+	document.body.addEventListener("keyup", this.handleKeyUp);
 
 
     },
@@ -56,9 +69,7 @@ const escena2 = {
    
     initRaycaster: function(){
 
-	this.raycaster = new THREE.Raycaster();
-
-	window.document.addEventListener( 'mousemove', this.handleMouseMove);
+	//this.raycaster = new THREE.Raycaster();
 
     },
 
@@ -83,11 +94,28 @@ const escena2 = {
 	    
 	case 49:
 	    escena2.esfericas = true;
-	    escena2.aleatorias = false; 
+	    escena2.aleatorias = false;
+	  
+	    for(let i = 0; i < escena2.tam.length; i ++){
+		escena2.scene.remove(escena2.cube[i]);
+		escena2.scene.remove(escena2.cube[i].text);
+	    }
+	    
+	    escena2.addTextSpheres();
+
 	    break;
 	case 50:
 	    escena2.esfericas = false;
 	    escena2.aleatorias = true;
+	    
+	    for(let i = 0; i < escena2.tam.length; i ++){
+		escena2.scene.remove(escena2.cube[i]);
+		escena2.scene.remove(escena2.cube[i].text);
+	    }
+	    
+	    escena2.addTextSpheres();
+
+
 	    break;
 	}
 
@@ -95,12 +123,15 @@ const escena2 = {
 
     },
 
-    onDocumentMouseMove: function( event ) {
+    handleMouseMove: function( event ) {
 	
 	event.preventDefault();
+
+        escena2.mouse.clientX = event.clientX
+        escena2.mouse.clientY = event.clientY
+        escena2.mouse.x = (event.clientX / window.innerWidth) * 2 -1;
+        escena2.mouse.y = -(event.clientY / window.innerHeight) * 2 +1;
 	
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 	
     },
 
@@ -163,8 +194,6 @@ const escena2 = {
 
 	var theta, radius = 100;
 
-	var cube;
-	
 	var params = {
 	    envMap: 'HDR',
 	    roughness: 0.3,
@@ -173,44 +202,79 @@ const escena2 = {
 	    debug: false
 	};
 
-	var material = new THREE.MeshStandardMaterial( {
-	    color: 0xffffff,
-	    metalness: params.metalness,
-	    roughness: params.roughness
-	} );
 
-		
-      var loader = new THREE.FontLoader();      
 
-	loader.load( '/static/fonts/helvetiker_bold.typeface.json', function ( font ) {
-
-	    for(var i = 0; i < escena2.tam.length ; i ++){   	       
+	var loader = new THREE.FontLoader();      
 	
+	loader.load( '/static/fonts/helvetiker_bold.typeface.json', function ( font ) {
+	    
+	    for(var i = 0; i < escena2.tam.length ; i ++){   	       
+		
 		var geometry = new THREE.SphereGeometry(20, 20, 20);
-	      
-		// geometry.center();
+
+		var material = new THREE.MeshStandardMaterial( {
+		    color: 0xffffff,
+		    metalness: params.metalness,
+		    roughness: params.roughness
+		} );
+
+		// var material = new THREE.MeshLambertMaterial( { color: 0xffffff });
+
+					   // geometry.center();
 		//geometry.computeVertexNormals(); 
 		geometry.computeBoundingBox();
 		
 		escena2.cube[i] = new THREE.Mesh( geometry, material );
+		//escena2.cube = new THREE.Mesh( geometry, material );
+
+		escena2.scene.add(escena2.cube[i]);
+		
+		var shapes = font.generateShapes( escena2.tweets[i], escena2.tam[i]/80 );
+		var geometry2 = new THREE.ShapeBufferGeometry( shapes );
+
+		// geometry.center(); 
+		// geometry2.computeBoundingBox();
+		
+		escena2.text[i] = new THREE.Mesh( geometry2, material );
+	
 
 		//var posX, posY, posZ;
 
 		if(escena2.aleatorias){
 
+		    /*
 		    var posX, posY, posZ;
 
 		    posX = Math.random() * 20 - 10;
 		    posY = Math.random() * 20 - 10;
 		    posZ = Math.random() * 20 - 10;
 
-		    escena2.cube[i].position.x = posX * (0.25*escena2.tam[i]);
-		    escena2.cube[i].position.y = posY * (0.25*escena2.tam[i]);
-		    escena2.cube[i].position.z = posZ * (0.25*escena2.tam[i]);
+		    escena2.cube[i].position.x = posX ;
+		    escena2.cube[i].position.y = posY ;
+		    escena2.cube[i].position.z = posZ ;
 		    
-		    escena2.cube[i].scale.x = escena2.tam[i]/150;
-		    escena2.cube[i].scale.y = escena2.tam[i]/150;
-		    escena2.cube[i].scale.z = escena2.tam[i]/150;
+		    escena2.cube[i].scale.x = escena2.tam[i]/1500;
+		    escena2.cube[i].scale.y = escena2.tam[i]/1500;
+		    escena2.cube[i].scale.z = escena2.tam[i]/1500;
+		    */
+
+		    var posX, posY, posZ;
+		    
+		    var theta1 = Math.random() * (Math.PI*2);
+		    var theta2 = Math.random() * (Math.PI*2); 
+		    
+		    posX = Math.cos(theta1) * Math.cos(theta2);
+		    posY = Math.sin(theta1);
+		    posZ = Math.cos(theta1) * Math.sin(theta2);
+		    
+		    escena2.cube[i].position.x = posX * (Math.random() * 100);
+		    escena2.cube[i].position.y = posY * (Math.random() * 100);
+		    escena2.cube[i].position.z = posZ * (Math.random() * 100);
+		    
+		    escena2.cube[i].scale.x = escena2.tam[i]/2500;
+		    escena2.cube[i].scale.y = escena2.tam[i]/2500;
+		    escena2.cube[i].scale.z = escena2.tam[i]/2500;
+	
 		    
 		}
 		
@@ -240,25 +304,15 @@ const escena2 = {
 		//cube[i].matrixAutoUpdate = false;
 		// cube[i].updateMatrix();
 		
-		escena2.scene.add(escena2.cube[i]);
 		
-		var shapes = font.generateShapes( escena2.tweets[i], escena2.tam[i]/80 );
-	//			var shapes = font.generateShapes( escena2.tweets[i], escena2.tam[i]/4 );
+		escena2.text[i].position.x = posX * (0.5 * escena2.tam[i]);
+		escena2.text[i].position.y = posY * (0.5 * escena2.tam[i]);
+		escena2.text[i].position.z = posZ * (0.5 * escena2.tam[i]);
+		
+		//escena2.text[i].lookAt(0, 0, 0); 
 
-		
-		var geometry2 = new THREE.ShapeBufferGeometry( shapes );
-
-		// geometry.center(); 
-		geometry2.computeBoundingBox();
-		
-		escena2.text[i] = new THREE.Mesh( geometry2, material );
-		
-		escena2.text[i].position.x = posX * (0.5 * escena2.tam[i])+ (escena2.tam[i]/32);
-		escena2.text[i].position.y = posY * (0.5 * escena2.tam[i])+ (escena2.tam[i]/32);
-		escena2.text[i].position.z = posZ * (0.5 * escena2.tam[i])+ (escena2.tam[i]/32);
-		
-		escena2.text[i].lookAt(0, 0, 0); 
-		
+		escena2.cube[i].text = escena2.text[i];
+		// pregunta, esto podría estar fuera del for sin los indices?
 		// escena2.scene.add(escena2.text[i]);
 		
 	    }
@@ -271,24 +325,24 @@ const escena2 = {
     handleResize: function(event){
 
 	escena2.camera.aspect = window.innerWidth / window.innerHeight;
-	escena2.camera.updateProjectionMatrix();
+	// escena2.camera.updateProjectionMatrix();
 	escena2.renderer.setSize( window.innerWidth, window.innerHeight );
 	
     },
 
+   
+
     animate: function(){
 	//escena2.controls.update(); 
+
 	requestAnimationFrame( escena2.animate );
+	escena2.render(); 
+	
+    },
 
-	// raycaster.setFromCamera( mouse, camera );
-
-	// escena2.controls.update(); 
-
-	//escena2.camera.rotateX(0.0002);
-	// escena2.camera.rotateY(0.0003);
+    render: function(){
 
 	var time = Date.now() * 0.0005;
-
 	
 	//escena2.camera.position.z += 0.05;
 	escena2.light1.position.x = Math.sin( time * 0.7 ) * 30;
@@ -307,10 +361,53 @@ const escena2 = {
 	escena2.light4.position.y = Math.cos( time * 0.7 ) * 40;
 	escena2.light4.position.z = Math.sin( time * 0.5 ) * 30;
 
-	escena2.camera.updateProjectionMatrix();
+	
+	escena2.camera.updateMatrixWorld(); 
+
+	var INTERSECTED;
+
+	// escena2.raycaster = new THREE.Raycaster();
+
+	escena2.raycaster.setFromCamera( escena2.mouse, escena2.camera );
+	//let intersects = escena2.raycaster.intersectObjects( escena2.scene.children );
+	let intersects = escena2.raycaster.intersectObjects( escena2.scene.children );
+	
+	if ( intersects.length > 0 ) {
+
+	    if ( escena2.INTERSECTED != intersects[ 0 ].object ) {
+		
+		if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+		
+		INTERSECTED = intersects[ 0 ].object;
+		INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+		INTERSECTED.material.emissive.setHex( 0xffffff );
+
+		if(INTERSECTED.text){
+		
+		    escena2.scene.add(INTERSECTED.text);
+		    
+		}
+		
+	    }
+	    
+	} else {
+	    
+	    if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+	    
+	    INTERSECTED = null;
+	    
+	}
+
+
+	//if(!INTERSECTED ==0){ 
+	for(var i = 0; i < escena2.tam.length; i++){
+	    escena2.cube[i].text.lookAt(escena2.camera.position); 
+	}
+	// }
+	    
 
 	escena2.renderer.render( escena2.scene, escena2.camera );
-	
+
     },
 
     
