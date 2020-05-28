58b590e0bb0b245be01a9f154c8cb5045861c9bf
Dorian Velazquez <dorian.velazquez@digitalonus.com>
2020-05-27 21:01:49 -0500

    audio test

--- webserver/static/js/escena1.js
+++ webserver/static/js/escena1.js
@@ -116,7 +116,32 @@ const escena1 = {
 
 		window.document.addEventListener( 'mousemove', this.handleMouseMove);
 	},
+	initSound: function(){
+		// create an AudioListener and add it to the camera
+		const listener = new THREE.AudioListener();
+		camera.add( listener );
+		console.log("AAA asdf")
+		// create the PositionalAudio object (passing in the listener)
+		const sound = new THREE.PositionalAudio( listener );
 
+		// load a sound and set it as the PositionalAudio object's buffer
+		const audioLoader = new THREE.AudioLoader();
+		audioLoader.load( '/static/sounds/08monk.wav', function( buffer ) {
+			sound.setBuffer( buffer );
+			sound.setRefDistance( 20 );
+			sound.play();
+		});
+
+		// create an object for the sound to play from
+		const sphere = new THREE.SphereBufferGeometry( 20, 32, 16 );
+		const material = new THREE.MeshPhongMaterial( { color: 0xff2200 } );
+		const mesh = new THREE.Mesh( sphere, material );
+		scene.add( mesh );
+
+		// finally add the sound to the mesh
+		mesh.add( sound );
+		escena1.add(mesh)
+	},
 	addFloor: function(){
 
 		// draw floor
